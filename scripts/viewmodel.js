ce9ed31dd48971ecb74fb3ff4405e6d70b6bf916
mayurongithub <mayurarora@gmail.com>
2014-12-05 17:47:33 +0530

    bug fix

--- scripts/viewmodel.js
+++ scripts/viewmodel.js
@@ -1,14 +1,13 @@
 function createVM(tabObjects){
     var vm = (function() {
         var tabs = ko.observableArray(tabObjects);
-        var tabGlimpse = function(index) {           
-            chrome.windows.update(index.windowId,{focused:true},function(window) {
-            console.log("Window Id : " + window.id + " focused");                
-           });
-              chrome.tabs.update(index.id,{active:true},function(tab){
+        var tabGlimpse = function(index) {   
+             chrome.tabs.update(index.id,{active:true},function(tab){
                 console.log("switched to tab " + index.id);
             });
-           
+            chrome.windows.update(index.windowId,{focused:true},function(window) {
+            console.log("Window Id : " + window.id + " focused");                
+           });                        
         }        
         return {
             tabs:tabs,
